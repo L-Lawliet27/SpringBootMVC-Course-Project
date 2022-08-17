cc858d9a57c92ce9cd4beb4fe4aaddba5aaee57a
L-Lawliet27 <andressalazarcolina@gmail>
2022-08-17 16:51:26 +0200

    First Commit

---
+++ src/test/java/com/appsdeveloperblog/spring/mvc/SpringBootMVCProject/SpringBootMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.spring.mvc.SpringBootMVCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
